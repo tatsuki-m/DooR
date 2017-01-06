3baf460638427f5334b3590fd0b1bde065ae144a
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-06 12:44:54 +0900

    initial commit

---
+++ CMakeFiles/DooR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DooR.dir/main.cpp.o"
+  "DooR.pdb"
+  "DooR"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DooR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
